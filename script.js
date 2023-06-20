0425d4cded4e4159e01e83429d3765312794c8dc
Jhansi <95856797+jhans1-cs@users.noreply.github.com>
2023-06-20 15:49:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 function threeSum(arr, target) {
-// write your code here
-  
+  const result = [];
+
+  // Sorting the array in ascending order
+  arr.sort((a, b) => a - b);
+
+  const len = arr.length;
+
+  for (let i = 0; i < len - 2; i++) {
+    // Skip duplicates
+    if (i > 0 && arr[i] === arr[i - 1]) {
+      continue;
+    }
+
+    let left = i + 1;
+    let right = len - 1;
+
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+
+      if (sum === target) {
+        result.push([arr[i], arr[left], arr[right]]);
+        left++;
+        right--;
+
+        // Skip duplicates
+        while (left < right && arr[left] === arr[left - 1]) {
+          left++;
+        }
+        while (left < right && arr[right] === arr[right + 1]) {
+          right--;
+        }
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = threeSum;
+
